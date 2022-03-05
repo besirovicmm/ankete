bcb0a2ccf3e35aee1df71ef1e2f117c708e86732
Muhammed Besirovic <besirovicmm@gmail.com>
2022-03-05 16:18:21 +0100

    aaaa

--- frontend/src/Screen/Anketascreen.js
+++ frontend/src/Screen/Anketascreen.js
@@ -1,22 +1,22 @@
-import React, { useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { uzmiPitanja } from "../REdux/Slices/FetchData";
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { uzmiPitanja } from '../REdux/Slices/FetchData'
 
 const Anketascreen = () => {
-  const params = useParams();
-  const { id } = params;
-  const dispatch = useDispatch();
-  const anketeZasebno = useSelector((stanje) => stanje.anketa.anketeZasebno);
+  const params = useParams()
+  const { id } = params
+  const dispatch = useDispatch()
+  const anketeZasebno = useSelector((stanje) => stanje.anketa.anketeZasebno)
 
   useEffect(() => {
-    dispatch(uzmiPitanja(id));
-  }, []);
+    dispatch(uzmiPitanja(id))
+  }, [])
   return (
     <div>
       <h1>Anketa {id}</h1>
     </div>
-  );
-};
+  )
+}
 
-export default Anketascreen;
+export default Anketascreen
