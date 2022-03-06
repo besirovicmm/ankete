ae02ab3e14ff0082730ce5c02740d7bbf25f217b
Muhammed Besirovic <besirovicmm@gmail.com>
2022-03-06 21:40:36 +0100

    dodaj ankete korisnika po ID screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
-import HomeScreen from "./Screen/HomeScreen";
-import { Route, Routes } from "react-router-dom";
-import Anketascreen from "./Screen/Anketascreen";
-import SignUp from "./Screen/SignUp";
-import Login from "./Screen/Login";
+import HomeScreen from './Screen/HomeScreen'
+import { Route, Routes } from 'react-router-dom'
+import Anketascreen from './Screen/Anketascreen'
+import SignUp from './Screen/SignUp'
+import Login from './Screen/Login'
+import AnketeKorisnika from './Screen/AnketeKorisnika'
 
 function App() {
   return (
@@ -10,11 +11,12 @@ function App() {
       <Routes>
         <Route path="/" element={<HomeScreen />} />
         <Route path="/anketa/:id" element={<Anketascreen />} />
+        <Route path="/ankete-korisnika/" element={<AnketeKorisnika />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
